4b2a7a229151239c91f06ed5ac926918f4fd6c52
ay <allzip1117@gmail.com>
2023-04-09 21:48:26 +0900

    프로젝트 생성

---
+++ src/main/java/com/campus/board2023/Board2023Application.java
@@ -0,0 +1,13 @@
+package com.campus.board2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Board2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/campus/board2023/Board2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.board2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
